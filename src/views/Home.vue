9422de002091d5bf335953a74282cdab17831183
Kevin19960728 <903701039@qq.com>
2021-03-14 12:12:18 +0800

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,126 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="hello" style="background-color: #f1f5fe; height: 100vh">
+    <div
+      style="
+        width: 100%;
+        height: 300px;
+        background-color: #101044;
+        color: #fff;
+        line-height: 300px;
+      "
+    >
+      通过质押HSwap LP1231321231231321321231
+    </div>
+    <div class="plate pr">
+        <div class="pa total" >锁定总值0.00 USDT</div>
+      <div class="dsp-center" style="padding: 40px 200px">
+        <div style="background-color: #fff; padding: 20px 40px; border-radius: 10px">
+          <div>HBO-USDT_LP</div>
+          <div style="font-size: 12px; margin-top: 10px">
+            赚取{{initReward}}HBO(每天)
+          </div>
+          <div style="font-size: 12px; margin-bottom: 10px">
+            赚取{{monthReward}}HBO(每月)
+          </div>
+          <div style="font-size: 12px; margin: 20px 0">{{totalSupply}} USDT</div>
+          <div
+            style="
+              font-size: 12px;
+              margin-bottom: 10px;
+              padding: 5px 10px;
+              border: 1px solid #2d58cb;
+              background-color: #2d58cb;
+              color: #fff;
+              border-radius: 15px;
+            "
+            class="dsp-between"
+          >
+            <span>APY</span>
+            <span>0%</span>
+          </div>
+          <div
+            style="
+              border: 1px solid #85ca98;
+              border-radius: 15px;
+              padding: 3px 0;
+            "
+            @click="$router.push({ path: '/mine' })"
+          >
+            选择
+          </div>
+        </div>
+        <div style="background-color: #fff; padding: 20px 40px; border-radius: 10px">
+          <div>HBO-USDT_LP</div>
+          <div style="font-size: 12px; margin-top: 10px">
+            赚取0.0000 HBO(每天)
+          </div>
+          <div style="font-size: 12px; margin-bottom: 10px">
+            赚取0.0000 HBO(每天)
+          </div>
+          <div style="font-size: 12px; margin: 20px 0">0.00 USDT</div>
+          <div
+            style="
+              font-size: 12px;
+              margin-bottom: 10px;
+              padding: 5px 10px;
+              border: 1px solid #2d58cb;
+              background-color: #2d58cb;
+              color: #fff;
+              border-radius: 15px;
+            "
+            class="dsp-between"
+          >
+            <span>APY</span>
+            <span>0%</span>
+          </div>
+          <div
+            style="
+              border: 1px solid #85ca98;
+              border-radius: 15px;
+              padding: 3px 0;
+            "
+          >
+            选择
+          </div>
+        </div>
+        <div style="background-color: #fff; padding: 20px 40px; border-radius: 10px">
+          <div>HBO-USDT_LP</div>
+          <div style="font-size: 12px; margin-top: 10px">
+            赚取0.0000 HBO(每天)
+          </div>
+          <div style="font-size: 12px; margin-bottom: 10px">
+            赚取0.0000 HBO(每天)
+          </div>
+          <div style="font-size: 12px; margin: 20px 0">0.00 USDT</div>
+          <div
+            style="
+              font-size: 12px;
+              margin-bottom: 10px;
+              padding: 5px 10px;
+              border: 1px solid #2d58cb;
+              background-color: #2d58cb;
+              color: #fff;
+              border-radius: 15px;
+            "
+            class="dsp-between"
+          >
+            <span>APY</span>
+            <span>0%</span>
+          </div>
+          <div
+            style="
+              border: 1px solid #85ca98;
+              border-radius: 15px;
+              padding: 3px 0;
+            "
+          >
+            选择
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   </div>
 </template>
 
@@ -19,6 +137,9 @@ export default {
   data() {
     return {
       account: "",
+      totalSupply: "",
+      initReward: "",
+      monthReward: ""
     }
   },
   methods: {
@@ -30,10 +151,13 @@ export default {
         // 查询 huiwanUsdtLoop 池子初始奖励数量 57600000000000000000000
         cfg.getInitreward(function(res) {
             console.log("当前奖励数量：" + res);
+            that.initReward = res / 1000000000000000000;
+            that.monthReward = that.initReward * 30 ;
         });
         // 查询项目方 huiwanUsdtLoop 池子里面的 lp 总数量
         cfg.getTotalSupply(function(res) {
             console.log("当前池子 lp 总量：" + res);
+            that.totalSupply = res / 1000000000000000000;
         });
         // 查询某个用户在 huiwanUsdtLoop 池子中的当前收益
         cfg.getEarned(res, function(res) {
@@ -46,7 +170,7 @@ export default {
         // 查询 mdex 中配对合约拥有 usdtToken 的数量
         cfg.getBalanceFromUsdtTokenContract("0x9Df49c31ac2C9C82cCEa6315F4F90eeae83A4182", function(res) {
             console.log("mdex 中配对合约拥有 usdtToken 数量：" + res);
-            });
+        });
       });
     },
   },
@@ -55,3 +179,49 @@ export default {
   }
 }
 </script>
+<style scoped>
+.plate {
+  width: 1200px;
+  margin: 0 auto;
+}
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.dsp-center {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.dsp-between {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.pr{
+    position: relative;
+}
+.pa{
+    position: absolute;
+}
+.total{
+    left: 50%;
+    top: -32px;
+    transform: translateX(-50%);
+    background-color: #66be7f;
+    color:#fff;
+    padding: 20px;
+    border-radius:6px
+}
+</style>
---
+++ src/views/mine.vue
@@ -0,0 +1,128 @@
+<template>
+  <div style="background-color:#f1f5fe;height:100vh">
+     <div
+     class="dsp-center"
+      style="
+        width: 100%;
+        height: 300px;
+        background-color: #101044;
+        color: #fff;
+
+      "
+    >
+    <div>
+        <h2>HBO矿池</h2>
+      <p>注意：我们现在仅支持Heco，亲勿转让以太<br />坊主网token</p>
+    </div>
+      
+    </div>
+    <div class="plate">
+        <div class="dsp-center pr" style="top:-30px">
+            <div style="margin-right:10px;height:166px;width:250px;padding:20px 30px;background-color:#fff;border-radius:10px">
+                <div>您的余额</div>
+                <div style="margin:20px 0">{{initReward}} HBO</div>
+                <div style="border-radius:15px;border:1px solid #bfe4c9;padding:3px 0;width:100%">获取所有Token</div>
+            </div>
+            <div style="height:166px;width:250px;padding:20px 30px;background-color:#fff;border-radius:10px">
+                <div>Mdex LP 抵押</div>
+                <div>HBO-USDT-LP</div>
+                <div style="margin:20px 0">0.0000</div>
+                <div style="border-radius:15px;border:1px solid #bfe4c9;padding:3px 0;width:100%" @click="openShow">授权</div>
+            </div>
+        </div>
+    </div>
+    <div style="width:100%;height:100%;top:0;left:0" class="pa dsp-center" v-show="openshow">
+      <div @click="dowmshow" style="background-color:#000;opacity:0.8;width:100%;height:100%;top:0;left:0" class="pa"></div>
+      <div style="background-color:#fff;padding:30px;opacity:1;top:50%;left:50%;transform: translate(-50% -50%);" class="pa">
+        <div>付款钱包:</div>
+        <div>收款地址:</div>
+        <div>矿工费:</div>
+        <div>操作名称:</div>
+        <div>授权数量:</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import cfg from "@/apis/nodeServer.js";
+
+
+export default {
+  data(){
+    return{
+      openshow:false,
+      initReward: null
+    }
+  },
+  methods:{
+    openShow(){
+      this.openshow = !this.openshow;
+      this.getauthorization()
+    },
+    getauthorization(){
+      console.log('获取授权')
+      
+    },
+    dowmshow(){
+      this.openshow = false;
+    },
+    init(){
+        let that = this;
+        cfg.init(function(res){
+            cfg.getInitreward(function(res){
+                that.initReward = res /1000000000000000000;
+            })
+        })
+    }
+  },
+  created() {
+      this.init();
+  }
+
+}
+</script>
+
+<style>
+.plate {
+  width: 1200px;
+  margin: 0 auto;
+}
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.dsp-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.dsp-around {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.dsp-between {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.pr{
+    position: relative;
+}
+.pa{
+    position: absolute;
+}
+</style>
